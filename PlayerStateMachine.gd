bc0ded3728e66a7836116ade2030ab9b83df45b3
Ash Gop <46041897+AshieG@users.noreply.github.com>
2019-07-21 00:52:04 -0400

    Create PlayerStateMachine.gd

---
+++ PlayerStateMachine.gd
@@ -0,0 +1,17 @@
+extends "res://StateMachine.gd"
+
+func _ready():
+	pass
+
+func _state_logic(delta):
+	pass
+
+func _get_transition(delta):
+	return null
+	
+func _enter_state(new_state, old_state):
+	pass
+	
+func _exit_state(old_state, new_state):
+	pass
+	
